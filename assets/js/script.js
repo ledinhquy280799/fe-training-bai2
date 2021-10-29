78e2d267d80a32dc5e1d1281ce4551d4394d3367
DESKTOP-V4M5IG8\QuyLAPTOP <ledinhquy280799@gmail.com>
2021-10-29 14:23:52 +0700

    update navbar sticky

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,13 @@ $(document).ready(function() {
 $(window).on('load', function() {
     $(".loader").fadeOut(1000);
 });
+
+$(window).scroll(function() {
+    console.log($(document).scrollTop());
+    if ($(document).scrollTop() > 50) {
+        $(".top-header").css("z-index", "90");
+        $(".top-header").css("background-color", "rgba(0, 0, 0, 0.8)")
+    } else {
+        $(".top-header").css("background-color", "rgb(255, 255, 255, 0.4)");
+    }
+});
